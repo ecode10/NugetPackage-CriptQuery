84b625767d8cc77afd2470fd57b80f8ffc1715ac
Mauricio Junior <mauricio.junior@gmail.com>
2021-10-23 14:36:19 -0500

    add project
    add project

---
+++ NugetPackage-CriptQuery/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using CriptQuery;
+
+namespace NugetPackage_CriptQuery.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var security = new SecureQueryString();
+            security["emailDoUsuario"] = "test@gmail.com";
+
+            Response.Redirect($"Privacy?id={security}");
+        }
+    }
+}
---
+++ NugetPackage-CriptQuery/Pages/Privacy.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using CriptQuery;
+
+namespace NugetPackage_CriptQuery.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrEmpty(Request.Query["id"]))
+                Response.Redirect("index");
+
+            var _id = Request.Query["id"];
+
+            var security = new SecureQueryString(_id);
+            var email = security["emailDoUsuario"];
+
+            Console.Write(email);
+        }
+    }
+}
